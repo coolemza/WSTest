5ca7660c0c9f444e58ebcf0541a246849f2a19c5
lc <leonid@coolemza.com>
2019-02-17 22:57:30 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,43 @@
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.features.websocket.WebSockets
+import io.ktor.client.features.websocket.ws
+import io.ktor.client.features.websocket.wss
+import io.ktor.http.cio.websocket.Frame
+import io.ktor.http.cio.websocket.readText
+import kotlinx.coroutines.channels.filterNotNull
+import kotlinx.coroutines.channels.map
+import kotlinx.coroutines.runBlocking
+import org.json.simple.JSONObject
+
+enum class Parameters {
+    key,
+    port,
+
+    initDb,
+    saveState,
+    debug
+}
+
+suspend fun main() {
+    try {
+        println("start")
+
+        val client = HttpClient(CIO).config { install(WebSockets) }
+
+        client.wss(host = "echo.websocket.org") {
+            send(Frame.Text("Hello World"))
+
+            for (message in incoming.map { it as? Frame.Text }.filterNotNull()) {
+                println(message.readText())
+            }
+        }
+
+        println("start finished")
+
+    } catch (e: Exception) {
+        e.printStackTrace()
+        println("stopped abnormal")
+    }
+}
